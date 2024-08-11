0e8abc77c677a05fb0f87d74c78da732ebdbc5a4
Carlos Fdez. Llamas <hello@sirikon.me>
2024-08-11 13:45:20 +0200

    wip

---
+++ modules/static-site/build-site.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+BUILD_ID="$(openssl rand -hex 16)"
+BUILD_WORKSPACE="/srv/srkbz/static-sites/${DOMAIN}/builds/${BUILD_ID}"
+SITE_LIVE="/srv/srkbz/static-sites/${DOMAIN}/live"
+
+mkdir -p "${BUILD_WORKSPACE}"
+
+(
+    cd "${BUILD_WORKSPACE}"
+    git init
+    git remote add origin "${REPOSITORY}"
+    git fetch origin "${COMMIT}"
+    git reset --hard "${COMMIT}"
+
+    if [ -f "Dockerfile" ]; then
+        docker build -t "site_${BUILD_ID}" .
+        container_id=$(docker create "site_${BUILD_ID}")
+        rm -rf "${SITE_LIVE}"
+        docker cp "${container_id}:/dist" "${SITE_LIVE}"
+        docker rm -f "${container_id}"
+        docker rmi "site_${BUILD_ID}"
+    else
+        rm -rf "${SITE_LIVE}"
+        cp -r "dist" "${SITE_LIVE}"
+    fi
+)
+
+rm -rf "${BUILD_WORKSPACE}"
