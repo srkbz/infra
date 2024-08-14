ec53bb41b38067096e16e4e45a589c17159e54d3
Carlos Fdez. Llamas <hello@sirikon.me>
2024-08-14 19:46:46 +0200

    wip

---
+++ modules/webhook/write-config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import json
+from os import listdir
+from os.path import isdir, join
+
+webhooks = []
+
+if isdir(".cache/webhook/conf"):
+    for item in listdir(".cache/webhook/conf"):
+        item_path = join(".cache/webhook/conf", item)
+        with open(item_path, "r") as f:
+            webhooks = [*webhooks, *json.load(f)]
+
+print(webhooks)
