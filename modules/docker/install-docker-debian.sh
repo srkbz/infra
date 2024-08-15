db376d17abd24f2d10e6e7305d63b3173401186d
Carlos Fdez. Llamas <hello@sirikon.me>
2024-08-15 15:25:39 +0200

    some fixes

--- modules/docker/install-docker-debian.sh
+++ modules/docker/install-docker-debian.sh
@@ -9,8 +9,8 @@ curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/doc
 chmod a+r /etc/apt/keyrings/docker.asc
 
 echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-    tee /etc/apt/sources.list.d/docker.list >/dev/null
+  tee /etc/apt/sources.list.d/docker.list >/dev/null
 flock .cache/apt/lock apt-get update
-flock .cache/apt/lock apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+flock .cache/apt/lock apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
