ffa43b6abe6c1381cf2b103e0b5a6a2715bb627e
Carlos Fdez. Llamas <hello@sirikon.me>
2024-08-15 13:45:38 +0200

    wip

--- modules/secrets/secrets-download.sh
+++ modules/secrets/secrets-download.sh
@@ -6,4 +6,5 @@ chmod -R 700 .
 umask 077
 curl -Lo "secrets.zip" "${SECRETS_URL}"
 unzip -o 'secrets.zip' || true
+chmod 700 secrets.kdbx
 echo "${SECRETS_PASSWORD}" | keepassxc-cli export --quiet secrets.kdbx >secrets.xml
