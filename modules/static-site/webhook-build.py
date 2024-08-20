0c8b27f4e8ad48c90c0c96293c64d7dd90d58b6d
Carlos Fdez. Llamas <hello@sirikon.me>
2024-08-20 11:33:26 +0200

    wip

--- modules/static-site/webhook-build.py
+++ modules/static-site/webhook-build.py
@@ -7,7 +7,7 @@ DOMAIN = environ.get("DOMAIN")
 BRANCH = environ.get("BRANCH")
 WEBHOOK_SECRET = environ.get("WEBHOOK_SECRET")
 TASKFILE_DIR = environ.get("TASKFILE_DIR")
-SRKBZ_INFRA_ROOT = environ.get("SRKBZ_INFRA_ROOT")
+TASKW_ROOT = environ.get("TASKW_ROOT")
 
 if WEBHOOK_SECRET is None:
     raise Exception("WEBHOOK_SECRET is not configured")
@@ -20,7 +20,7 @@ with open(f".cache/webhook/conf/static-site_{DOMAIN}.json", "w") as f:
                 "id": f"static-site/{DOMAIN}",
                 "execute-command": f"{TASKFILE_DIR}/webhook-run.sh",
                 "pass-arguments-to-command": [{"source": "string", "name": DOMAIN}],
-                "command-working-directory": SRKBZ_INFRA_ROOT,
+                "command-working-directory": TASKW_ROOT,
                 "trigger-rule": {
                     "and": [
                         {
--- modules/static-site/webhook-run.sh
+++ modules/static-site/webhook-run.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 rm -f "/srv/srkbz/static-sites/${1}/TARGET_COMMIT"
-touch /srv/srkbz/triggers/APPLY
+touch .state/taskw/triggers/APPLY
