f30ba82c5d709023c3b593e5d83dc48eee2a3c02
Carlos Fdez. Llamas <hello@sirikon.me>
2024-10-19 19:32:06 +0200

    wip

--- modules/vpn/configure.py
+++ modules/vpn/configure.py
@@ -39,11 +39,22 @@ def main():
 
 
 def server_configure():
-    if not exists("/opt/vpn/private.key"):
-        makedirs("/opt/vpn", exist_ok=True)
+    if not exists("/opt/vpn/server/private.key"):
+        makedirs("/opt/vpn/server", exist_ok=True)
         run(
             ["bash", "-c", "wg genkey | tee private.key | wg pubkey>public.key"],
-            cwd="/opt/vpn/",
+            cwd="/opt/vpn/server",
+        )
+
+    with open("/opt/vpn/server/wg0.conf", "w") as f:
+        for line in server_wireguard_config():
+            f.write(line + "\n")
+
+    if not exists("/opt/vpn/home-gateway/private.key"):
+        makedirs("/opt/vpn/home-gateway", exist_ok=True)
+        run(
+            ["bash", "-c", "wg genkey | tee private.key | wg pubkey>public.key"],
+            cwd="/opt/vpn/home-gateway",
         )
 
     for client_name, client_ip in CLIENTS:
@@ -68,11 +79,11 @@ def client_home_lan_wireguard_config(client_name, client_ip):
     yield "PrivateKey = " + read_file(
         join("/opt/vpn/clients", client_name, "private.key")
     ).strip()
-    yield "Address = " + str(client_ip) + "/32"
+    yield f"Address = {client_ip}/32"
     yield ""
     yield "[Peer]"
     yield "PublicKey = " + read_file("/opt/vpn/public.key").strip()
-    yield f"AllowedIPs = {VPN_NETWORK}/24, {HOME_NETWORK}/24"
+    yield f"AllowedIPs = {VPN_NETWORK}, {HOME_NETWORK}"
     yield f"Endpoint = {SERVER_PUBLIC_ADDRESS}:{WG_PORT}"
 
 
@@ -80,21 +91,23 @@ def server_wireguard_config():
     yield "[Interface]"
     yield f"Address = {SERVER_IP}/{VPN_NETWORK.prefixlen}"
     yield f"ListenPort = {WG_PORT}"
-    yield "PrivateKey = " + read_file("iface-keys/private").strip()
+    yield "PrivateKey = " + read_file("/opt/vpn/private.key").strip()
     yield ""
     yield "PostUp = iptables -A FORWARD -i wg0 -o wg0 -j ACCEPT"
     yield "PostDown = iptables -D FORWARD -i wg0 -o wg0 -j ACCEPT"
     yield ""
     yield "# Home Gateway"
     yield "[Peer]"
-    yield "PublicKey = " + read_file("peer-data/home-gateway.public-key").strip()
+    yield "PublicKey = " + read_file("/opt/vpn/home-gateway/public.key").strip()
     yield f"AllowedIPs = {HOME_GATEWAY_IP}/32,{HOME_NETWORK}"
     yield ""
-    for name, ip in CLIENTS:
-        yield f"# Client: {name}"
+    for client_name, client_ip in CLIENTS:
+        yield f"# Client: {client_name}"
         yield "[Peer]"
-        yield "PublicKey = " + read_file(f"peer-data/{name}.public-key").strip()
-        yield f"AllowedIPs = {ip}/32"
+        yield "PublicKey = " + read_file(
+            f"/opt/vpn/clients/{client_name}/public.key"
+        ).strip()
+        yield f"AllowedIPs = {client_ip}/32"
 
 
 def home_gateway_configure():
