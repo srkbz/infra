348122e1510aa86872166f7bf6f42f8c8bb8bb3f
Carlos Fdez. Llamas <hello@sirikon.me>
2024-10-19 18:51:12 +0200

    pre download custom ca

--- modules/uptime-kuma/scripts/start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Add missing CA
-curl 'https://cacerts.digicert.com/ThawteTLSRSACAG1.crt.pem' >/usr/local/share/ca-certificates/ThawteTLSRSACAG1.crt
-update-ca-certificates
-
-export NODE_EXTRA_CA_CERTS="/usr/local/share/ca-certificates/ThawteTLSRSACAG1.crt"
-exec node server/server.js
