63a181d4c2d8c064acc1dd048e6ace0af7d12a7c
Carlos Fdez. Llamas <hello@sirikon.me>
2024-11-21 22:36:25 +0100

    remove stuff

--- modules/secrets/secrets-xml-to-dir.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-rm -rf ./*
-chmod -R 700 .
-umask 077
-
-"${TASKFILE_DIR}/secrets-xml-to-dir.py" "$@"
--- modules/static-site/webhook-run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-rm -f "/srv/srkbz/static-sites/${1}/TARGET_COMMIT"
-date >/opt/taskw/triggers/RUN
