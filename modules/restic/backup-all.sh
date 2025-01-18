901a853dd9a112cb5b9564b9879913799f2728a8
Carlos Fdez. Llamas <hello@sirikon.me>
2025-01-18 20:36:00 +0100

    wip

--- modules/restic/backup-all.sh
+++ modules/restic/backup-all.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+SECRETS_FILE="$1"
+
+. "$SECRETS_FILE"
+
 for file in ./conf/*; do
     backup_name="$(basename "$file")"
     backup_location="$(cat "$file")"
+    backup_repo="${BASE_URL}/${BUCKET_NAME}/${backup_name}"
+    export AWS_ACCESS_KEY_ID="$KEY_ID"
+    export AWS_SECRET_ACCESS_KEY="$APP_KEY"
     echo "- $backup_name"
     echo "  $backup_location"
+
+    set -x
+
+    set +e
+    restic -r "$backup_repo" check --insecure-no-password
+    check_result=$?
+    set -e
+    if [ "$check_result" != 0 ]; then
+        restic -r "$backup_repo" init --insecure-no-password
+    fi
+
+    restic -r "$backup_repo" backup --insecure-no-password "$backup_location"
 done
