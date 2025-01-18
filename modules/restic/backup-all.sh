dfb16cbdb5947cace34ed9e833c83d7e790dab76
Carlos Fdez. Llamas <hello@sirikon.me>
2025-01-18 20:12:56 +0100

    wip

--- modules/restic/backup-all.sh
+++ modules/restic/backup-all.sh
@@ -3,5 +3,7 @@ set -euo pipefail
 
 for file in ./conf/*; do
     backup_name="$(basename "$file")"
+    backup_location="$(cat "$file")"
     echo "- $backup_name"
+    echo "  $backup_location"
 done
