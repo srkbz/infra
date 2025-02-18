6420f3c309356a25618150aeb9f2a01b9c3e6fa4
Carlos Fdez. Llamas <hello@sirikon.me>
2025-02-18 23:34:43 +0100

    wip

---
+++ modules/secrets/generate-env-op.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from os import environ
+from os.path import join
+from subprocess import run, PIPE
+from sys import argv
+import json
+
+SECRETS_HOME = environ.get("SECRETS_HOME")
+
+with open(join(SECRETS_HOME, "secrets.json"), "r") as f:
+    secrets = json.load(f)
+
+task_id = environ.get("EBRO_TASK_ID")
+
+expr = f'filter(tasks, .id == "{task_id}")[0].labels | toJSON()'
+labels_cmd = run(
+    [environ.get("EBRO_BIN"), "-i", "--query", expr],
+    cwd=environ.get("EBRO_ROOT"),
+    stdout=PIPE,
+)
+labels = json.loads(str(labels_cmd.stdout))
+
+print(labels)
