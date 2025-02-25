e9aa4f12e434fe3dc1959fa0af14ee86e79e1b34
Carlos Fdez. Llamas <hello@sirikon.me>
2025-02-25 23:19:51 +0100

    wip

--- modules/static-site/webhook-build.py
+++ modules/static-site/webhook-build.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import json
-from sys import argv
 from os import environ, getcwd
 
 DOMAIN = environ.get("DOMAIN")
@@ -12,38 +11,35 @@ SITE_HOME = environ.get("SITE_HOME")
 if WEBHOOK_SECRET is None:
     raise Exception("WEBHOOK_SECRET is not configured")
 
-with open(argv[1], "w") as f:
-    json.dump(
-        [
-            {
-                "id": f"static-site/{DOMAIN}",
-                "execute-command": f"{getcwd()}/webhook-run.sh",
-                "pass-arguments-to-command": [{"source": "string", "name": SITE_HOME}],
-                "command-working-directory": getcwd(),
-                "trigger-rule": {
-                    "and": [
-                        {
-                            "match": {
-                                "type": "payload-hmac-sha1",
-                                "secret": WEBHOOK_SECRET,
-                                "parameter": {
-                                    "source": "header",
-                                    "name": "X-Hub-Signature",
-                                },
-                            }
-                        },
-                        {
-                            "match": {
-                                "type": "value",
-                                "value": f"refs/heads/{BRANCH}",
-                                "parameter": {"source": "payload", "name": "ref"},
-                            }
-                        },
-                    ]
-                },
-            }
-        ],
-        f,
-        indent=2,
-    )
-    f.write("\n")
+json.dumps(
+    [
+        {
+            "id": f"static-site/{DOMAIN}",
+            "execute-command": f"{getcwd()}/webhook-run.sh",
+            "pass-arguments-to-command": [{"source": "string", "name": SITE_HOME}],
+            "command-working-directory": getcwd(),
+            "trigger-rule": {
+                "and": [
+                    {
+                        "match": {
+                            "type": "payload-hmac-sha1",
+                            "secret": WEBHOOK_SECRET,
+                            "parameter": {
+                                "source": "header",
+                                "name": "X-Hub-Signature",
+                            },
+                        }
+                    },
+                    {
+                        "match": {
+                            "type": "value",
+                            "value": f"refs/heads/{BRANCH}",
+                            "parameter": {"source": "payload", "name": "ref"},
+                        }
+                    },
+                ]
+            },
+        }
+    ],
+    indent=2,
+)
