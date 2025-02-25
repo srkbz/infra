83bdaa3c599160e61c2e469d3d9a918125fde58e
Carlos Fdez. Llamas <hello@sirikon.me>
2025-02-25 23:23:09 +0100

    wip

--- modules/static-site/webhook-build.py
+++ modules/static-site/webhook-build.py
@@ -11,35 +11,37 @@ SITE_HOME = environ.get("SITE_HOME")
 if WEBHOOK_SECRET is None:
     raise Exception("WEBHOOK_SECRET is not configured")
 
-json.dumps(
-    [
-        {
-            "id": f"static-site/{DOMAIN}",
-            "execute-command": f"{getcwd()}/webhook-run.sh",
-            "pass-arguments-to-command": [{"source": "string", "name": SITE_HOME}],
-            "command-working-directory": getcwd(),
-            "trigger-rule": {
-                "and": [
-                    {
-                        "match": {
-                            "type": "payload-hmac-sha1",
-                            "secret": WEBHOOK_SECRET,
-                            "parameter": {
-                                "source": "header",
-                                "name": "X-Hub-Signature",
-                            },
-                        }
-                    },
-                    {
-                        "match": {
-                            "type": "value",
-                            "value": f"refs/heads/{BRANCH}",
-                            "parameter": {"source": "payload", "name": "ref"},
-                        }
-                    },
-                ]
-            },
-        }
-    ],
-    indent=2,
+print(
+    json.dumps(
+        [
+            {
+                "id": f"static-site/{DOMAIN}",
+                "execute-command": f"{getcwd()}/webhook-run.sh",
+                "pass-arguments-to-command": [{"source": "string", "name": SITE_HOME}],
+                "command-working-directory": getcwd(),
+                "trigger-rule": {
+                    "and": [
+                        {
+                            "match": {
+                                "type": "payload-hmac-sha1",
+                                "secret": WEBHOOK_SECRET,
+                                "parameter": {
+                                    "source": "header",
+                                    "name": "X-Hub-Signature",
+                                },
+                            }
+                        },
+                        {
+                            "match": {
+                                "type": "value",
+                                "value": f"refs/heads/{BRANCH}",
+                                "parameter": {"source": "payload", "name": "ref"},
+                            }
+                        },
+                    ]
+                },
+            }
+        ],
+        indent=2,
+    )
 )
