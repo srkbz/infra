cd02637e71eaacd9730884f3dba0cd347f116389
Carlos Fdez. Llamas <hello@sirikon.me>
2025-04-23 23:44:29 +0200

    wip

---
+++ modules/apt/control.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import subprocess
+import sys
+import os
+
+
+def main():
+    lines = [
+        "Package: " + os.environ.get("METAPKG_NAME"),
+        "Version: 0.0.0",
+        "Architecture: all",
+        "Description: Metapackage containing all the packages needed",
+        "Depends: " + ", ".join(packages()),
+    ]
+    print("\n".join(lines))
+
+
+def packages() -> list[str]:
+    cmd = subprocess.run(
+        [
+            os.environ.get("EBRO_BIN"),
+            "-i",
+            "--query",
+            'tasks | filter("apt.packages" in .labels) | map(.labels["apt.packages"]) | join("\n")',
+        ],
+        cwd=os.environ.get("EBRO_ROOT"),
+        stdin=None,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=True,
+    )
+    output: str = cmd.stdout.encode("utf-8")
+    return output.splitlines()
--- modules/apt/packages.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-cd "${EBRO_ROOT}"
-"${EBRO_BIN}" -i --query 'tasks | filter("apt.packages" in .labels) | map(.labels["apt.packages"]) | join("\n")'
