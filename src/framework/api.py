bb9933cb5ae486192eae8ee9ad202d0d066b9f24
Carlos Fdez. Llamas <hello@sirikon.me>
2025-05-03 12:01:52 +0200

    wip

---
+++ src/framework/api.py
@@ -0,0 +1,26 @@
+from contextvars import ContextVar
+from contextlib import contextmanager
+
+from framework.runner import Runner
+from framework.task import Task
+
+
+__runner = ContextVar[Runner]("runner", default=None)
+
+
+@contextmanager
+def runner():
+    r = Runner()
+    token = __runner.set(r)
+    yield r
+    r.run()
+    __runner.reset(token)
+
+
+def task(requires: list[callable] = [], required_by: list[callable] = []):
+    def decorator(func):
+        task = Task(func=func, requires=requires, required_by=required_by)
+        __runner.get().add_task(task)
+        return task
+
+    return decorator
