04732288af0f26c5a3b75a0da0c4f7a5822bac03
Carlos Fdez. Llamas <hello@sirikon.me>
2025-05-11 01:55:28 +0200

    autoload

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
+import os
+import importlib
+
 from framework.runner import runner
-import modules.apt
+
+for module in os.listdir(os.path.join(os.path.dirname(__file__), "modules")):
+    importlib.import_module("modules." + module)
+
 
 runner.run()
