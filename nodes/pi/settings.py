02a115be5115e6b43f1459f41828acbbac067ba2
Carlos Fdez. Llamas <hello@sirikon.me>
2025-05-12 22:42:22 +0200

    wip

--- nodes/pi/settings.py
+++ nodes/pi/settings.py
@@ -1,5 +1,3 @@
-APT_PACKAGES = ["vim"]
-
 DIRECTORIES = {
     "public": {"path": "/srv/public", "owner": ["root", "root"], "perm": "755"},
 }
--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ from platform import node
 
 CACHE_DIR = join(getcwd(), ".cache")
 STATE_DIR = join(getcwd(), ".state")
+APT_PACKAGES = ["vim", "htop", "btop", "iftop", "iotop"]
 
 match node():
     case "pi":
