6f3c6083d97ec7a6cd7659a60ad2d3efee09eeb8
Carlos Fdez. Llamas <hello@sirikon.me>
2025-05-12 22:46:25 +0200

    wip

--- settings.py
+++ settings.py
@@ -1,11 +1,14 @@
-from os import getcwd
-from os.path import join
+import importlib
+import sys
+from os import getcwd, listdir
+from os.path import join, dirname
 from platform import node
 
 CACHE_DIR = join(getcwd(), ".cache")
 STATE_DIR = join(getcwd(), ".state")
 APT_PACKAGES = ["vim", "htop", "btop", "iftop", "iotop"]
 
-match node():
-    case "pi":
-        from nodes.pi.settings import *
+
+node_settings = importlib.import_module("nodes." + node() + ".settings")
+for key in node_settings:
+    setattr(sys.modules[__name__], key, node_settings[key])
