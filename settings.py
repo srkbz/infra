3ea4a48e51490e62cbd47dd516cd729377e70d93
Carlos Fdez. Llamas <hello@sirikon.me>
2025-05-15 01:10:53 +0200

    wip

--- settings.py
+++ settings.py
@@ -4,15 +4,14 @@ from os import getcwd, listdir
 from os.path import join, dirname
 from platform import node
 
-CACHE_DIR = join(getcwd(), ".cache")
-STATE_DIR = join(getcwd(), ".state")
-
-
 node_settings = importlib.import_module(
     "nodes." + node().replace("-", "_") + ".settings"
 )
 for key in dir(node_settings):
     setattr(sys.modules[__name__], key, getattr(node_settings, key))
+
+CACHE_DIR = getattr(sys.modules[__name__], "CACHE_DIR", join(getcwd(), ".cache"))
+STATE_DIR = getattr(sys.modules[__name__], "STATE_DIR", join(getcwd(), ".state"))
 
 APT_PACKAGES = getattr(sys.modules[__name__], "APT_PACKAGES", []) + [
     "vim",
