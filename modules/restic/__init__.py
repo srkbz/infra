2fa624c89ba4438a1cec95c78cf9f4143d3456b2
Carlos Fdez. Llamas <hello@sirikon.me>
2025-05-24 22:53:41 +0200

    fix

--- modules/restic/__init__.py
+++ modules/restic/__init__.py
@@ -1,3 +1,4 @@
+import platform
 from os import makedirs
 from os.path import isfile, join, dirname
 
@@ -9,17 +10,20 @@ import settings
 
 ENABLED = getattr(settings, "RESTIC_ENABLED", False)
 VERSION = getattr(settings, "RESTIC_VERSION", "0.18.0")
-VARIANT = "amd64"
 BACKUPS = getattr(settings, "RESTIC_BACKUPS", {})
+
+
+_VARIANTS = {"x86_64": "amd64", "aarch64": "arm64"}
+_VARIANT = _VARIANTS[platform.machine()]
 
 _CACHE_DIR = settings.CACHE_DIR
 
 _version_dir = join(_CACHE_DIR, "restic", "versions", VERSION)
 _archive = join(_version_dir, "archive.bz2")
 _archive_verified = join(_version_dir, "archive.verified")
-_archive_url = f"https://github.com/restic/restic/releases/download/v{VERSION}/restic_{VERSION}_{VARIANT}.bz2"
+_archive_url = f"https://github.com/restic/restic/releases/download/v{VERSION}/restic_{VERSION}_{_VARIANT}.bz2"
 _archive_sums = join(dirname(__file__), "versions", VERSION, "SHA256SUMS")
-_bin = join(_version_dir, f"restic_{VERSION}_{VARIANT}")
+_bin = join(_version_dir, f"restic_{VERSION}_{_VARIANT}")
 
 
 @task()
@@ -30,12 +34,12 @@ def install(dry_run: bool):
         shell(f"curl --fail --location --output '{_archive}' '{_archive_url}'")
     if not isfile(_archive_verified):
         assert not dry_run
-        shell(f"cat '{_archive_sums}' | grep '{VARIANT}' | sha256sum --check")
+        shell(f"cat '{_archive_sums}' | grep '{_VARIANT}' | sha256sum --check")
         shell(f"touch '{_archive_verified}'")
     if not isfile(_bin):
         assert not dry_run
         shell(
-            f"bzip2 -d 'restic_{VERSION}_{VARIANT}.bz2'",
+            f"bzip2 -d 'restic_{VERSION}_{_VARIANT}.bz2'",
             cwd=dirname(_archive),
         )
         shell(f"chmod +x '{_bin}'")
