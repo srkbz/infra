94dcd69d089a2715506464e05cd8bec68cad03d3
Carlos Fdez. Llamas <hello@sirikon.me>
2025-05-24 22:56:06 +0200

    fixes

--- modules/restic/__init__.py
+++ modules/restic/__init__.py
@@ -13,7 +13,7 @@ VERSION = getattr(settings, "RESTIC_VERSION", "0.18.0")
 BACKUPS = getattr(settings, "RESTIC_BACKUPS", {})
 
 
-_VARIANTS = {"x86_64": "amd64", "aarch64": "arm64"}
+_VARIANTS = {"x86_64": "linux_amd64", "aarch64": "linux_arm64"}
 _VARIANT = _VARIANTS[platform.machine()]
 
 _CACHE_DIR = settings.CACHE_DIR
@@ -21,7 +21,7 @@ _CACHE_DIR = settings.CACHE_DIR
 _version_dir = join(_CACHE_DIR, "restic", "versions", VERSION)
 _archive = join(_version_dir, "archive.bz2")
 _archive_verified = join(_version_dir, "archive.verified")
-_archive_url = f"https://github.com/restic/restic/releases/download/v{VERSION}/restic_{VERSION}_linux_{_VARIANT}.bz2"
+_archive_url = f"https://github.com/restic/restic/releases/download/v{VERSION}/restic_{VERSION}_{_VARIANT}.bz2"
 _archive_sums = join(dirname(__file__), "versions", VERSION, "SHA256SUMS")
 _bin = join(_version_dir, f"restic_{VERSION}_{_VARIANT}")
 
@@ -34,7 +34,10 @@ def install(dry_run: bool):
         shell(f"curl --fail --location --output '{_archive}' '{_archive_url}'")
     if not isfile(_archive_verified):
         assert not dry_run
-        shell(f"cat '{_archive_sums}' | grep '{_VARIANT}' | sha256sum --check")
+        shell(
+            f"cat '{_archive_sums}' | grep '{_VARIANT}' | sha256sum --check",
+            cwd=dirname(_archive),
+        )
         shell(f"touch '{_archive_verified}'")
     if not isfile(_bin):
         assert not dry_run
