bb0be38b85d83b33a420a8f790f1b9b0d33d6873
Carlos Fdez. Llamas <hello@sirikon.me>
2025-06-12 20:14:32 +0200

    wip

---
+++ modules/caddy/__init__.py
@@ -0,0 +1,34 @@
+import platform
+from os import makedirs
+from os.path import isfile, isdir, join, dirname
+
+from framework.api import task
+from framework.utils.shell import shell
+
+
+import settings
+
+
+ENABLED = getattr(settings, "CADDY_ENABLED", False)
+VERSION = getattr(settings, "CADDY_VERSION", "2.10.0")
+
+_ARCHS = {"x86_64": "amd64", "aarch64": "arm64"}
+_VARIANT = "linux_" + _ARCHS[platform.machine()]
+
+_cache_dir = join(settings.CACHE_DIR, "caddy")
+_version_dir = join(_cache_dir, "versions", VERSION)
+_archive = join(_version_dir, f"caddy.deb")
+_archive_url = f"https://github.com/caddyserver/caddy/releases/download/v{VERSION}/caddy_{VERSION}_{_VARIANT}.deb"
+
+
+def install(dry_run: bool):
+    if not isfile(_archive):
+        assert not dry_run
+        makedirs(dirname(_archive), exist_ok=True)
+        shell(f"curl --fail --location --output '{_archive}' '{_archive_url}'")
+
+    if shell("command -v caddy", check=False, echo=False).exit_code != 0 or not shell(
+        "caddy --version", echo=False, captureStdout=True
+    ).stdout.startswith(f"v{VERSION} "):
+        assert not dry_run
+        shell(f"apt-get install -y --allow-downgrades '{_archive}'")
