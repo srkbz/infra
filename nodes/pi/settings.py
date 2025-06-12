6c48cb3790ea8fd8243a8062e1a856de2531b34c
Carlos Fdez. Llamas <hello@sirikon.me>
2025-06-12 20:52:18 +0200

    disable caddy

--- nodes/pi/settings.py
+++ nodes/pi/settings.py
@@ -8,7 +8,7 @@ MINIDLNA_ENABLED = True
 MINIDLNA_FRIENDLY_NAME = "Raspberry Pi"
 MINIDLNA_DIRECTORY_ID = "public"
 
-CADDY_ENABLED = True
+CADDY_ENABLED = False
 CADDY_CADDYFILES = [
     textwrap.dedent(
         """
