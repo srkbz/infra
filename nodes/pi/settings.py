bf7e37c3c1cf00a111ba7e334a8db2aec47a699e
Carlos Fdez. Llamas <hello@sirikon.me>
2025-06-13 09:35:46 +0200

    wip

--- nodes/pi/settings.py
+++ nodes/pi/settings.py
@@ -8,5 +8,14 @@ MINIDLNA_DIRECTORY_ID = "public"
 
 STATIC_SITES_ENABLED = True
 STATIC_SITES = {
-    "sirikon.me": {"repository": "https://github.com/sirikon/sirikon.me.git"}
+    "sirikon.me": {"repository": "https://github.com/sirikon/sirikon.me.git"},
+    "osoondo.com": {"repository": "https://github.com/sirikon/osoondo.com.git"},
+    "egin.sirikon.me": {"repository": "https://github.com/sirikon/egin.git"},
+    "ebro.sirikon.me": {
+        "repository": "https://github.com/sirikon/ebro.git",
+        "build_script": "./meta/docker/build.sh website",
+        "directory": "out/website",
+    },
+    "astenagusia.eus": {"repository": "https://github.com/sirikon/astenagusia.git"},
+    "2048.sirikon.me": {"repository": "https://github.com/sirikon/2048.git"},
 }
