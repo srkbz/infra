fc4bda6648798973be298e8e55d207a9d2d7e9da
Carlos Fdez. Llamas <hello@sirikon.me>
2025-06-14 11:37:08 +0200

    facade

---
+++ nodes/facade/settings.py
@@ -0,0 +1,15 @@
+CADDY_ENABLED = True
+
+STATIC_SITES_ENABLED = True
+STATIC_SITES = {
+    "sirikon.me": {"repository": "https://github.com/sirikon/sirikon.me.git"},
+    "osoondo.com": {"repository": "https://github.com/sirikon/osoondo.com.git"},
+    "egin.sirikon.me": {"repository": "https://github.com/sirikon/egin.git"},
+    "ebro.sirikon.me": {
+        "repository": "https://github.com/sirikon/ebro.git",
+        "build_script": "./meta/docker/build.sh website",
+        "directory": "out/website",
+    },
+    "astenagusia.eus": {"repository": "https://github.com/sirikon/astenagusia.git"},
+    "2048.sirikon.me": {"repository": "https://github.com/sirikon/2048.git"},
+}
--- nodes/pi/settings.py
+++ nodes/pi/settings.py
@@ -5,17 +5,3 @@ DIRECTORIES = {
 MINIDLNA_ENABLED = True
 MINIDLNA_FRIENDLY_NAME = "Raspberry Pi"
 MINIDLNA_DIRECTORY_ID = "public"
-
-STATIC_SITES_ENABLED = False
-STATIC_SITES = {
-    "sirikon.me": {"repository": "https://github.com/sirikon/sirikon.me.git"},
-    "osoondo.com": {"repository": "https://github.com/sirikon/osoondo.com.git"},
-    "egin.sirikon.me": {"repository": "https://github.com/sirikon/egin.git"},
-    "ebro.sirikon.me": {
-        "repository": "https://github.com/sirikon/ebro.git",
-        "build_script": "./meta/docker/build.sh website",
-        "directory": "out/website",
-    },
-    "astenagusia.eus": {"repository": "https://github.com/sirikon/astenagusia.git"},
-    "2048.sirikon.me": {"repository": "https://github.com/sirikon/2048.git"},
-}
