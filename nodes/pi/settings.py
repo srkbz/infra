9938308bb5adcc84cdf0d77c64f5315f03990787
Carlos Fdez. Llamas <hello@sirikon.me>
2025-06-15 13:49:38 +0200

    wip

--- nodes/pi/settings.py
+++ nodes/pi/settings.py
@@ -1,6 +1,16 @@
+from framework.utils.fs import read_file
+
 DIRECTORIES = {
     "public": {"path": "/srv/public", "owner": ["root", "root"], "perm": "755"},
 }
 
 MINIDLNA_DIRECTORY_ID = "public"
 MINIDLNA_FRIENDLY_NAME = "Raspberry Pi"
+
+VPN_PROFILE = "vault"
+VPN_VAULT_PRIVATE_KEY = read_file(
+    ".secrets/VPN_VAULT_PRIVATE_KEY", must_exist=True
+).strip()
+VPN_FACADE_PUBLIC_KEY = read_file(
+    ".secrets/VPN_FACADE_PUBLIC_KEY", must_exist=True
+).strip()
