fd7c20a6abfd03611c6a0645908128a288fded38
Carlos Fdez. Llamas <hello@sirikon.me>
2025-06-17 11:44:53 +0200

    notes

---
+++ modules/tv/__init__.py
@@ -0,0 +1,12 @@
+# Sway
+    # mod key is Home
+    # 1920x1080 60hz
+    # menu opens on Home + OK
+        # blueman-manager
+        # pavucontrol
+        # moonlight
+    # navigate workspace on Home + Number
+    # switch between windows with Home + directional pad
+    # close current window with Home + Q
+    # shutdown/restart menu with Home + Esc
+# auto-login and launch sway on boot
