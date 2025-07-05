d5fc3aa768f74f6cd241dc50acb702c5db7b7558
Carlos Fdez. Llamas <hello@sirikon.me>
2025-07-05 13:32:34 +0200

    wip

--- modules/tv/__init__.py
+++ modules/tv/__init__.py
@@ -1,12 +1,75 @@
 # Sway
-    # mod key is Home
-    # 1920x1080 60hz
-    # menu opens on Home + OK
-        # blueman-manager
-        # pavucontrol
-        # moonlight
-    # navigate workspace on Home + Number
-    # switch between windows with Home + directional pad
-    # close current window with Home + Q
-    # shutdown/restart menu with Home + Esc
+# mod key is Home
+# 1920x1080 60hz
+# menu opens on Home + OK
+# blueman-manager
+# pavucontrol
+# moonlight
+# navigate workspace on Home + Number
+# switch between windows with Home + directional pad
+# close current window with Home + Q
+# shutdown/restart menu with Home + Esc
 # auto-login and launch sway on boot
+from os.path import isfile
+from os import makedirs
+import textwrap
+
+from framework.api import task
+from framework.utils.fs import read_file, write_file
+from framework.utils.shell import shell
+
+from modules import apt
+
+import settings
+
+ENABLED = getattr(settings, "TV_ENABLED", False)
+USER = getattr(settings, "TV_USER", "tv")
+
+if ENABLED:
+    apt.config.add_packages("sway", "pavucontrol", "blueman")
+
+
+def _setup(dry_run: bool):
+    if (
+        shell(f"getent passwd '{USER}' >/dev/null", check=False, echo=False).exit_code
+        != 0
+    ):
+        assert not dry_run
+        shell(f"useradd --create-home '{USER}'")
+
+    if not isfile("/etc/systemd/system/getty@tty1.service.d/autologin.conf"):
+        assert not dry_run
+        makedirs("/etc/systemd/system/getty@tty1.service.d", exist_ok=True)
+        write_file(
+            "/etc/systemd/system/getty@tty1.service.d/autologin.conf",
+            textwrap.dedent(
+                f"""
+                [Service]
+                ExecStart=
+                ExecStart=-/sbin/agetty -o '-p -f -- \\u' --noclear --autologin '{USER}' %I /bin/bash
+                """
+            ).lstrip(),
+        )
+
+
+def _cleanup(dry_run: bool):
+    pass
+
+
+def _needs_cleanup():
+    try:
+        _cleanup(dry_run=True)
+        return False
+    except:
+        return True
+
+
+@task()
+def setup(dry_run: bool):
+    if ENABLED:
+        _setup(dry_run)
+    else:
+        _cleanup(dry_run)
+
+
+setup.enabled(lambda: ENABLED or _needs_cleanup())
--- nodes/vault/settings.py
+++ nodes/vault/settings.py
@@ -1,9 +1,1 @@
-from framework.utils.fs import read_file
-
-VPN_PROFILE = "vault"
-VPN_VAULT_PRIVATE_KEY = read_file(
-    ".secrets/VPN_VAULT_PRIVATE_KEY", must_exist=True
-).strip()
-VPN_FACADE_PUBLIC_KEY = read_file(
-    ".secrets/VPN_FACADE_PUBLIC_KEY", must_exist=True
-).strip()
+TV_ENABLED = True
